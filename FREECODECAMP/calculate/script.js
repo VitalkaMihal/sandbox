4c84a952a94dcf2d90b8d5dc32e66528ecd5f37d
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-12-28 22:17:53 +0300

    feat(calculate): add step 5

--- FREECODECAMP/calculate/script.js
+++ FREECODECAMP/calculate/script.js
@@ -1,4 +1,7 @@
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
-    const array = value.split('')
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => {
+  Number(el)
+  });
   }
