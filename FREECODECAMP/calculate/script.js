10b448354dcac3149c593a11684b3bed6aa364dd
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-12-29 23:20:50 +0300

    feat(calculate): add step 15

--- FREECODECAMP/calculate/script.js
+++ FREECODECAMP/calculate/script.js
@@ -5,8 +5,4 @@ const calculate = () => {
   
   }
 
-  const getMean = (array) => {
-    const sum = array.reduce((acc, el) => acc + el, 0);
-    const mean = sum / array.length;
-    return mean;
-  }
+  const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
