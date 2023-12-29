0d7d1b840c56a1150f0adec0e7f7f43d46e86dbc
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-12-29 23:21:53 +0300

    feat(calculate): add step 16

--- FREECODECAMP/calculate/script.js
+++ FREECODECAMP/calculate/script.js
@@ -2,7 +2,7 @@ const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
+    const mean = getMean(numbers);
   }
 
   const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
