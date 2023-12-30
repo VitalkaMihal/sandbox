737589ebb4feae9365aeada434f5251a7e328517
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-12-30 12:41:55 +0300

    feat(calculate): add step 26

--- FREECODECAMP/calculate/script.js
+++ FREECODECAMP/calculate/script.js
@@ -13,7 +13,7 @@ const calculate = () => {
   }
 
   const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
-  const getMode = (array) => {};
+
   const getMedian = (array) => {
     const sorted = array.sort((a, b) => a - b);
     const median =
@@ -21,4 +21,8 @@ const calculate = () => {
       ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
       : sorted[Math.floor(array.length / 2)];
       return median;
+  }
+
+  const getMode = (array) => {
+    const counts = {};
   }
