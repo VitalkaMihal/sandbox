ef2dba020c2dd8f54d2ab5d8b9040cc59228f48b
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2024-01-10 20:14:35 +0300

    feat(react): add step 3

--- FREECODECAMP/react/react.js
+++ FREECODECAMP/react/react.js
@@ -1,11 +1,7 @@
 const JSX = (
-  <div>
-    <h1>hello</h1>
-    <p> me</p>
-    <ul>
-      <li>one</li>
-      <li>two</li>
-      <li>three</li>
-    </ul>
-  </div>
-);
+    <div>
+      <h1>This is a block of JSX</h1>
+      {/* hello */}
+      <p>Here's a subtitle</p>
+    </div>
+  );
