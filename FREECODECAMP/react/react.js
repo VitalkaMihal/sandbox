d047604076b49d60586baed3392e78286b0445ac
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2024-01-12 20:04:45 +0300

    feat(react): add step 13

--- FREECODECAMP/react/react.js
+++ FREECODECAMP/react/react.js
@@ -1,19 +1,15 @@
-class TypesOfFood extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      return (
-        <div>
-          <h1>Types of Food:</h1>
-          {/* Change code below this line */}
-          <Fruits />
-          <Vegetables />
-          {/* Change code above this line */}
-        </div>
-      );
-    }
-  };
-  
-  // Change code below this line
-  ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
+// Change code below this line
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'))
