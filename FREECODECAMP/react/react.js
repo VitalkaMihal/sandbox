89aa91137493c23897b308fbd8ba255c369e9144
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2024-01-16 23:18:23 +0300

    feat(react) add step 40

--- FREECODECAMP/react/react.js
+++ FREECODECAMP/react/react.js
@@ -7,24 +7,17 @@ class MyComponent extends React.Component {
     this.toggleDisplay = this.toggleDisplay.bind(this);
   }
   toggleDisplay() {
-    this.setState((state) => ({
+    this.setState(state => ({
       display: !state.display
     }));
   }
   render() {
     // Change code below this line
-    if (this.state.display) {return (
+    return (
        <div>
          <button onClick={this.toggleDisplay}>Toggle Display</button>
-         <h1>Displayed!</h1>
-       </div>
-    );} else {
-      return (
-       <div>
-         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         { this.state.display  && <h1>Displayed!</h1>}
        </div>
     );
-    }
-    
   }
 };
